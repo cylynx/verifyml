0faaf16035e9993f52eed7ab35035df63c83aed8
Jason <jason.ho@lynxanalytics.com>
2021-11-03 17:15:15 +0800

    finally got venv working on vercel and vercel.sh

--- vercel.sh
+++ vercel.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
-# script to set up and activate a python virtual env in vercel
+# script to set up a python virtual env in vercel
 
 amazon-linux-extras install python3.8
+
+# docs.sh uses python3
 alias python3=python3.8
 
+# install and activate virtual env
 pip3.8 install virtualenv
 virtualenv venv
 source venv/bin/activate
-python3 --version
+
+# install requirements
 pip3.8 install -r requirements.txt
+
+# NOTE: venv does not persist across vercel's install and build commands, 
+# so use `source venv/bin/activate && ./docs.sh build` for build
