5fc32facc6046ec17bd2a0852d3d5d67b6c5adee
Jason <jason.ho@lynxanalytics.com>
2021-11-03 18:35:23 +0800

    simplify vercel.sh

--- vercel.sh
+++ vercel.sh
@@ -6,13 +6,5 @@ amazon-linux-extras install python3.8
 # docs.sh uses python3
 alias python3=python3.8
 
-# install and activate virtual env
-pip3.8 install virtualenv
-virtualenv venv
-source venv/bin/activate
-
 # install requirements
 pip3.8 install -r requirements.txt
-
-# NOTE: venv does not persist across vercel's install and build commands, 
-# so use `source venv/bin/activate && ./docs.sh build` for build
