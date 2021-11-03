2efad41e963a015b7b45653fbce4c2a6cfdfe553
Jason <jason.ho@lynxanalytics.com>
2021-11-03 16:56:18 +0800

    edit vercel.sh

--- vercel.sh
+++ vercel.sh
@@ -2,6 +2,8 @@
 # script to set up and activate a python virtual env in vercel
 
 amazon-linux-extras install python3.8
-python3.8 -m venv venv
+pip install virtualenv
+virtualenv venv
 source venv/bin/activate
-pip3.8 install -r requirements.txt
+python --version
+pip install -r requirements.txt
