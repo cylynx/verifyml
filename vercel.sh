95b0157cc0da5c397e7bdaca850421e5fab9403d
Jason <73581922+jasonho-lynx@users.noreply.github.com>
2021-11-05 09:55:32 +0800

    Multiple changes to docs (#36)
    * set search path for pydoc-markdown.yml
    
    * added development docs to pydoc-markdown.yml, changed readme URLs to absolute paths
    
    * jupyter notebooks working. switched to material theme
    
    * updated docs section of the readme
    
    * improved convert_md_to_ipynb.py, changed requirements.txt
    
    * set docs.sh to exit upon error, added pyYAML to requirements
    
    * add vercel.sh script to create venv on vercel deployment

---
+++ vercel.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# install script to set up a python virtual env in vercel
+
+amazon-linux-extras install python3.8
+
+# install and activate virtual env
+pip3.8 install virtualenv
+virtualenv venv
+source venv/bin/activate
+
+# install requirements
+pip3.8 install -r requirements.txt
+
+# venv and packages do not persist across vercel's install and build commands?
+# use `source venv/bin/activate && ./docs.sh build` for build to ensure same env
