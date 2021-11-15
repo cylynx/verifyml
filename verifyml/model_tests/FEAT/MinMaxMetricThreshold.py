3987238ae909710bb9d9ccc2c34b29406a690cb7
Jason <jason.ho@lynxanalytics.com>
2021-11-15 14:48:10 +0800

    remove extra import, minor formatting

--- verifyml/model_tests/FEAT/MinMaxMetricThreshold.py
+++ verifyml/model_tests/FEAT/MinMaxMetricThreshold.py
@@ -57,16 +57,13 @@ class MinMaxMetricThreshold(ModelTest):
     test_desc: str = None
 
     def __post_init__(self):
+        lower_req = {"fpr", "fnr", "mse", "mae"}
+        higher_req = {"tpr", "tnr"}
 
-        lower_req_metrics = {"fpr", "fnr", "mse", "mae"}
-        higher_req_metrics = {"tpr", "tnr"}
+        if self.metric not in lower_req | higher_req:
+            raise ValueError(f"metric should be one of {lower_req | higher_req}.")
 
-        if self.metric not in lower_req_metrics | higher_req_metrics:
-            raise ValueError(
-                f"metric should be one of {lower_req_metrics | higher_req_metrics}."
-            )
-
-        req = "lower" if self.metric in lower_req_metrics else "higher"
+        req = "lower" if self.metric in lower_req else "higher"
 
         default_test_desc = inspect.cleandoc(
             f"""
